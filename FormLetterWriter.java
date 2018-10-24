0428cee713ecb11aecc5f4512253c76c8b0419b9
Natasha S Love <debora@inbox.ru>
2018-10-24 18:27:29 -0500

    Update FormLetterWriter.java

--- FormLetterWriter.java
+++ FormLetterWriter.java
@@ -13,14 +13,14 @@ public class FormLetterWriter{
 	
 	public static void displaySalutation(String lastName) {
 		
-		System.out.println("Dear Mr./Mrs "+ lastName);
+		System.out.println("Dear Mr./Mrs. "+ lastName);
 		System.out.println("Thank you for your recent order.");
 		System.out.println("_________________________________");
 	}
 	
 	public static void displaySalutation(String firstName, String lastName) {
 		
-		System.out.println("Dear Mr./Mrs "+ firstName + " " + lastName);
+		System.out.println("Dear Mr./Mrs. "+ firstName + " " + lastName);
 		System.out.println("Thank you for your recent order.");
 		System.out.println("_________________________________");
 	}
